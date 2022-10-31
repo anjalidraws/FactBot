dd1c880926ac10d3b6df75a55261173749d79a70
Abhishek Verma <abhishek201404@axiscolleges.in>
2022-10-31 12:33:43 +0530

    Fixed issue by adding a User-Agent

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ bot = telebot.TeleBot("bot_token")
 
 @bot.message_handler(commands=["fact"])
 def fact(message):
+    headers = {"User-Agent":"Google bot 1.0"}
     response = requests.get("https://catfact.ninja/fact").json()
     bot.send_message(message.chat.id, response["fact"])
 
